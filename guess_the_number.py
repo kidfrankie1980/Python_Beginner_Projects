f0dd11f2d0bf33487d47d246871008f5aa681455
ironheart93 <59081010+ironheart93@users.noreply.github.com>
2019-12-21 21:19:39 +0000

    Add files via upload

---
+++ guess_the_number.py
@@ -0,0 +1,17 @@
+import random
+
+
+golden_number = random.randint(0, 20)
+count = int(0)
+guess = int(3)
+
+while count != guess:
+    choice = int(input("Input a random number from 1 to 20: "))
+    if choice == golden_number:
+        print("Congratulations, you've found the golden number!")
+    elif choice < golden_number:
+        count += 1
+        print("Your number is too low! You have ", guess-count, "left!")
+    elif choice > golden_number:
+        count += 1
+        print("Your number is too large! You have ", guess-count, "left")
